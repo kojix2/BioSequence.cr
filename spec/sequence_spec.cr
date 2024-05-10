211bcd013b46d0b2e951cace039dd2a4d2384697
kojix2 <2xijok@gmail.com>
2024-05-10 10:04:43 +0900

    Add test

--- spec/sequence_spec.cr
+++ spec/sequence_spec.cr
@@ -9,4 +9,11 @@ describe Bio::Sequence do
     seq = T.normalize_sequence("ATGC")
     T.reverse_complement(seq).should eq(T.normalize_sequence("GCAT"))
   end
+
+  it "normalize_sequence" do
+    seq = T.normalize_sequence("ATGC")
+    String.new(seq).should eq("ATGC")
+    seq = T.normalize_sequence("atgc")
+    String.new(seq).should eq("ATGC")
+  end
 end
